a2814e011746e550e837cb2945cc9d630a2952ac
Harendra Singh Kuntal <harshkuntal7dec@gmail.com>
2026-08-25 10:27:37 +0530

    Time: 3 ms (98.19%), Space: 77.1 MB (85.89%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,12 +3,12 @@ class Solution {
         int left = 0;
         int right = height.length -1;
         int maxVal = 0;
-        while(left<right){
+        while(left < right){
             if(height[left] < height[right]){
-              maxVal = Math.max(maxVal, height[left] * (right - left));
-              left++;
-            }else{
-                maxVal = Math.max(maxVal, height[right] *(right-left));
+                maxVal = Math.max(maxVal, height[left] * (right - left));
+                left++;
+            } else {
+                maxVal = Math.max(maxVal, height[right] * (right - left));
                 right--;
             }
         }
